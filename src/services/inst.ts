8068b7e678bfc5f48c68b0386b8dd8c065d7fb4b
gel1ler <alex00713571@gmail.com>
2024-07-15 14:04:41 +0300

    obj-cover

--- src/services/inst.ts
+++ src/services/inst.ts
@@ -1,12 +1,10 @@
+'use server'
+
 export const getInfo = async () => {
-    // console.log(process.env.INSTAGRAM_KEY)
-
-    // const url = `https://graph.instagram.com/me?fields=id,username&access_token=${process.env.INSTAGRAM_KEY}`
-    const url = `https://graph.instagram.com/me?access_token=IGQWRQZA0EzUWZABUEVGbG1CaDNxOUJwRTM2OThzSndrMzAwdkJtQjFfbHZAvOGd6REpwY0NmNjNMeXgyb0tmeEVlek5fN2luTm1JRmRwYlpwTFRDOGF0QTZAJRGtpZAFRIUmd0clppV2RaVHRWeUxpV3pxZA0dEUkpqOUkZD`
-
-    const data = await fetch(url)
-    // console.log(data)
-
-    const info = await data.json()
-    console.log(info)
+  console.log(process.env.INSTAGRAM_KEY)
+  // const url = `https://graph.instagram.com/me?fields=id,username&access_token=${process.env.INSTAGRAM_KEY}`
+  const url = `https://graph.instagram.com/me&access_token=IGQWRPUm9MLU5JUk0za2lsR04wd0hBQmg3YW9rcWJ4dU9pb29fWTJYdXV3RXJNOHFBTGhUQXlUa3JNRlZAoX2FJMzlqY2F5ZAFFPS3h6M0lyX3BEZAzVMNDJzT2dKcEx6ZAUVKY3dVSThnVFhXT2lqanpRRDBsZA0lrcm8ZD`
+  const data = await fetch(url)
+  const info = await data.json()
+  console.log(info)
 }
